9b45955d7ac9389420cc95fdbe99d657c5867b79
Mehdi Haimmad <mehdihaimmad45@gmail.com>
2024-12-05 22:12:13 +0100

    Updating title on Pokemon View

--- pages/@slug/+data.tsx
+++ pages/@slug/+data.tsx
@@ -1,12 +1,18 @@
+import { useConfig } from "vike-react/useConfig";
 import { PageContextServer } from "vike/types";
 
 export type Data = Awaited<ReturnType<typeof data>>;
 
 export const data = async (pageContext: PageContextServer) => {
+    const config = useConfig();
     const response = await fetch(`https://pokedex.coda.memento-dev.fr/pokemon/${pageContext.routeParams.slug}`, {
         headers: {Authorization: `Bearer ${import.meta.env.API_KEY}`}
     });
     const pokemon = await (response.json());
+
+    config({
+        title: pokemon.current.name
+    })
 
     return pokemon.current;
 };
