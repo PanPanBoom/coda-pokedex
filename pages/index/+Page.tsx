2475a35f61f0e7c4df406889e4ae50c14a841b71
Mehdi Haimmad <mehdihaimmad45@gmail.com>
2024-12-05 13:43:40 +0100

    Added search and filter by types in the pokedex

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,16 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import { useData } from "vike-react/useData";
-import type { Data } from "./+data";
+import { data, type Data } from "./+data";
 import { Link } from "../../components/Link";
 
 export default function Page() {
-  const pokemons = useData<Data>();
+  const [pokemons, types] = useData<Data>();
+  const [pokemonsDisplayed, setPokemonsDisplayed] = useState(pokemons); 
+  const [input, setInput] = useState("");
+  const [selectedType, setSelectedType] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+
+    filterPokemons(e.target.value, selectedType)
+  }
+
+  const handleSelection = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedType(e.target.value);
+
+    filterPokemons(input, e.target.value);
+  }
+
+  const filterPokemons = async (name: String, typeParam: String) => {
+    let listPokemons = [];
+    listPokemons = pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(name.toLowerCase()));
+    
+    
+    if(typeParam.length < 1)
+    {    
+      setPokemonsDisplayed(listPokemons);
+      return;
+    }
+
+    console.log("test");
+
+    let listPokemonsObjects = await Promise.all(listPokemons.map(async (pokemon) => {
+      const response = await fetch(`https://pokedex.coda.memento-dev.fr/pokemon/${pokemon.slug}`, {
+        headers: {Authorization: `Bearer ${import.meta.env.API_KEY}`}
+      });
+      const json = await response.json();
+
+      return json.current;
+    }));
+
+    listPokemons = listPokemons.filter((pokemon, index) => {
+      let typeInData = false;
+      listPokemonsObjects[index].types.forEach(type => {
+        if(type.name === typeParam)
+          typeInData = true;
+      })
+
+      return typeInData;
+    });
+
+    setPokemonsDisplayed(listPokemons);
+  }
   
   return (
     <>
       <h1 className={"font-bold text-3xl pb-4"}>Pokédex</h1>
+      <input value={input} type="text" onChange={handleInputChange}/>
+      <select value={selectedType} onChange={handleSelection}>
+        <option></option>
+        {types.map(type => <option>{type.name}</option>)}
+      </select>
       <ul>
-        {pokemons.map((pokemon, index) => <li key={index}><Link href={`/${pokemon.slug}`}>{pokemon.name}</Link></li>)}
+        {pokemonsDisplayed.map((pokemon, index) => <li key={index}><Link href={`/${pokemon.slug}`}>{pokemon.name}</Link></li>)}
       </ul>
     </>
   );
--- pages/index/+data.tsx
+++ pages/index/+data.tsx
@@ -1,10 +1,15 @@
 export type Data = Awaited<ReturnType<typeof data>>;
 
 export const data = async () => {
-    const response = await fetch("https://pokedex.coda.memento-dev.fr/pokemon", {
+    const responsePokemons = await fetch("https://pokedex.coda.memento-dev.fr/pokemon", {
         headers: {Authorization: `Bearer ${import.meta.env.API_KEY}`}
     });
-    const pokemons = await response.json();
+    const pokemons = await responsePokemons.json();
 
-    return pokemons;
+    const responseTypes = await fetch("https://pokedex.coda.memento-dev.fr/type", {
+        headers: {Authorization: `Bearer ${import.meta.env.API_KEY}`}
+    });
+    const types = await responseTypes.json();
+
+    return [pokemons, types];
 };
